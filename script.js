5f348876f0800d05a805bdd9f584f83029a83e7e
DimAng2002 <153761671+DimAng2002@users.noreply.github.com>
2025-05-13 01:31:06 +0300

    Added a reset button, and the option to unclick a listing

--- script.js
+++ script.js
@@ -1,20 +1,36 @@
 function addText(){
     var items = document.querySelectorAll(".to_do");
-
+    
     items.forEach(item => {
         var h1Element = item.querySelector('h1');
-        item.addEventListener('click', (i) => {
-            i.currentTarget.style.backgroundColor = "red";
-            h1Element.textContent = "Clicked";
-        });
+        if(!item.hasAttribute('data-has-listener')) {
+            item.addEventListener('click', (i) => {
+                if (h1Element.textContent != "Clicked"){
+                    i.currentTarget.style.backgroundColor = "red";
+                    h1Element.textContent = "Clicked";
+                } else {
+                    i.currentTarget.style.backgroundColor = "aquamarine";
+                    h1Element.textContent = "";
+                }
+            });
+            item.setAttribute('data-has-listener', 'true');
+        }
     });
 }
 
+
 function addNew(){
     const parentElement = document.getElementById('container');
-    parentElement.insertAdjacentHTML('beforeend', "<div class = 'to_do'><h1></0h1></div>");
+    parentElement.insertAdjacentHTML('beforeend', "<div class = 'to_do'><h1></h1></div>");
     addText();
 
+}
+
+function resetListings(){
+    var items = document.querySelectorAll(".to_do");
+    items.forEach(item=>{
+        item.remove();
+    });
 }
 
 window.addEventListener('load', () => {
